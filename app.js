52683b08e6a17e17691524c88597b8dd7f4318a3
Abhishek-Malviya-07 <malviya.abhi3450@gmail.com>
2025-06-12 11:55:16 +0530

    adding a diff color for o and x and logic when there is no winner

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let mssgContainer = document.querySelector(".msg-container");
 
 let turnO = true;
 
-const winnPatterrns =[
+const winnPatterrns = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -18,61 +18,82 @@ const winnPatterrns =[
 ];
 
 boxes.forEach((box) => {
+    box.classList.remove("o-style", "x-style");
     box.addEventListener("click", () => {
-       if(turnO){
-        box.innerText = "0";
-        turnO = false;
-       }else{
-        box.innerText= "X";
-        turnO = true;
-       }
-       box.disabled = true;
-       checkWinner();
+
+        if (turnO) {
+            box.innerText = "O";
+            box.classList.add("o-style");
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            box.classList.add("x-style");
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
     });
 });
 
-const disableBtn = ()=>{
-    for(let box of boxes){
+const disableBtn = () => {
+    for (let box of boxes) {
         box.disabled = true;
     }
 }
 
-const enableBoxes = ()=>{
-    for(let box of boxes){
+const enableBoxes = () => {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
+        box.classList.remove("o-style", "x-style");
     }
 }
 
-const resetGame = () =>{
-     turnO = true;
-     enableBoxes();
-     mssgContainer.classList.add("hide");
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    mssgContainer.classList.add("hide");
 }
 
 const showWinner = (winner) => {
-  mssg.innerText = `Congrats , Winner  is ${winner}`;
-  mssgContainer.classList.remove("hide");
-  disableBtn();
+    mssg.innerText = `Congrats , Winner  is ${winner}`;
+
+    mssgContainer.classList.remove("hide");
+    disableBtn();
 }
 
-checkWinner = () => {
+const checkWinner = () => {
+    let winnerFound = false;
+
     winnPatterrns.forEach((pattern) => {
         let box1 = boxes[pattern[0]].innerText;
         let box2 = boxes[pattern[1]].innerText;
         let box3 = boxes[pattern[2]].innerText;
 
-        if(box1 === "" || box2 === "" || box3 === "") return;
-
-        if(box1 === box2 && box2 === box3){
-            showWinner(box1);
-           // alert(`Player ${box1} wins!`);
-            
+        if (box1 !== "" && box1 === box2 && box2 === box3) {
+            showWinner(box1);  // Display who won
+            winnerFound = true;
         }
     });
+
+    // Draw: All boxes filled and no winner
+    if (!winnerFound) {
+        let allFilled = true;
+        boxes.forEach((box) => {
+            if (box.innerText === "") {
+                allFilled = false;
+            }
+        });
+
+        if (allFilled) {
+            mssg.innerText = `It's a Draw!`;
+            mssgContainer.classList.remove("hide");
+            disableBtn();
+        }
+    }
 };
 
-newGameBtn.addEventListener("click" ,resetGame);
-resetBtn.addEventListener("click" , resetGame);
 
- 
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
